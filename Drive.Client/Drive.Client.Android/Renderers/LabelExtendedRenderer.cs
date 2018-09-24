c9ecdf762bf82bbe2ab33aad1465a6163204c13a
vadym.stetsyuk.89 <vadym.stetsyuk.89@gmail.com>
2018-09-24 17:22:29 +0300

    commit

---
+++ Drive.Client/Drive.Client.Android/Renderers/LabelExtendedRenderer.cs
@@ -0,0 +1,33 @@
+﻿using Android.Content;
+using Drive.Client.Controls;
+using Drive.Client.Droid.Renderers;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(LabelExtended), typeof(LabelExtendedRenderer))]
+namespace Drive.Client.Droid.Renderers {
+    class LabelExtendedRenderer : LabelRenderer {
+
+        public LabelExtendedRenderer(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e) {
+            base.OnElementChanged(e);
+
+            SetLetterSpacing();
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e) {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == LabelExtended.LetterSpacingProperty.PropertyName) {
+                SetLetterSpacing();
+                UpdateLayout();
+            }
+        }
+
+        private void SetLetterSpacing() {
+            Control.LetterSpacing = ((LabelExtended)Element).LetterSpacing;
+        }
+    }
+}
