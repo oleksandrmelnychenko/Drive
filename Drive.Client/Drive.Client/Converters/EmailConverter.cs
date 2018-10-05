ff4650dc12462e385d1d7f5cd2f136d51c085067
Vladimir Ivanov <ivanov_va@hotmail.com>
2018-10-05 11:03:50 +0300

    update

---
+++ Drive.Client/Drive.Client/Converters/EmailConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Drive.Client.Converters {
+    public class EmailConverter : IValueConverter {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            string email = value as string;
+            return !string.IsNullOrEmpty(email) ? email : "your@email.com";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
