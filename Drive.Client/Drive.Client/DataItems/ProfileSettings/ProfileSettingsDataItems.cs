83754ff196436134773a97864449cf8acd47a5e0
vadym.stetsyuk.89 <vadym.stetsyuk.89@gmail.com>
2018-10-05 14:26:17 +0300

    Language popup

---
+++ Drive.Client/Drive.Client/DataItems/ProfileSettings/ProfileSettingsDataItems.cs
@@ -0,0 +1,22 @@
+﻿using Drive.Client.Models.DataItems.ProfileSettings;
+using System.Collections.Generic;
+
+namespace Drive.Client.DataItems.ProfileSettings {
+    public class ProfileSettingsDataItems : IProfileSettingsDataItems {
+
+        private static readonly string _UKRAINIAN_LANGUAGE_TITLE = "Українська";
+        private static readonly string _ENGLISH_LANGUAGE_TITLE = "English";
+
+        public List<LanguageDataItem> BuildLanguageDataItems() =>
+            new List<LanguageDataItem>() {
+                new LanguageDataItem() {
+                    Language = LanguageInterface.Ukrainian,
+                    Title = _UKRAINIAN_LANGUAGE_TITLE
+                },
+                new LanguageDataItem() {
+                    Language = LanguageInterface.English,
+                    Title = _ENGLISH_LANGUAGE_TITLE
+                }
+            };
+    }
+}
---
+++ Drive.Client/Drive.Client/Models/DataItems/ProfileSettings/LanguageDataItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Drive.Client.Models.DataItems.ProfileSettings {
+    public class LanguageDataItem {
+
+        public string Title { get; set; }
+
+        public LanguageInterface Language { get; set; }
+    }
+}
