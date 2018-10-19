b97de29d4ff5315de6fe7b42863d45db46d6835b
Vladimir Ivanov <ivanov_va@hotmail.com>
2018-10-19 16:27:03 +0300

    temp update

---
+++ Drive.Client/Drive.Client/Views/BottomTabViews/Bookmark/SavedView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Drive.Client.Views.BottomTabViews.Bookmark
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SavedView : ContentView
+	{
+		public SavedView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
