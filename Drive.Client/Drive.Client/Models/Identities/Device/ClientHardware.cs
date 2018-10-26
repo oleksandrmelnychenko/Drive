4451f23b947c05bbe07a3e8fc7707206bb67043f
Vladimir Ivanov <ivanov_va@hotmail.com>
2018-10-26 13:54:26 +0300

    dev_microfix

--- Drive.Client/Drive.Client/Models/Identities/Device/ClientHardware.cs
+++ Drive.Client/Drive.Client/Models/Identities/Device/ClientHardware.cs
@@ -2,53 +2,7 @@
 using Plugin.DeviceInfo.Abstractions;
 
 namespace Drive.Client.Models.Identities.Device {
-    //public class ClientHardware {
-
-    //    [JsonProperty("Longitude")]
-    //    public double? Longitude { get; set; }
-
-    //    [JsonProperty("Latitude")]
-    //    public double? Latitude { get; set; }
-
-    //    [JsonProperty("DeviceId")]
-    //    public string DeviceId { get; set; }
-
-    //    [JsonProperty("TimestampUtc")]
-    //    public string TimestampUtc { get; set; }
-
-    //    [JsonProperty("Model")]
-    //    public string Model { get; set; }
-
-    //    [JsonProperty("Manufacturer")]
-    //    public string Manufacturer { get; set; }
-
-    //    [JsonProperty("DeviceName")]
-    //    public string DeviceName { get; set; }
-
-    //    [JsonProperty("Version")]
-    //    public string Version { get; set; }
-
-    //    [JsonProperty("VersionNumber")]
-    //    public string VersionNumber { get; set; }
-
-    //    [JsonProperty("AppVersion")]
-    //    public string AppVersion { get; set; }
-
-    //    [JsonProperty("AppBuild")]
-    //    public string AppBuild { get; set; }
-
-    //    [JsonProperty("Platform")]
-    //    public Platform Platform { get; set; }
-
-    //    [JsonProperty("Idiom")]
-    //    public Idiom Idiom { get; set; }
-
-    //    [JsonProperty("IsDevice")]
-    //    public bool IsDevice { get; set; }
-    //}
-
     public class ClientHardware {
-
         [JsonProperty("longitude")]
         public double? Longitude { get; set; }
 
