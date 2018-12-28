6fbb47f8dc9813c37921b3a6e179331c34e81653
vadym.stetsyuk.89 <vadym.stetsyuk.89@gmail.com>
2018-12-28 12:02:41 +0200

    Signal service, listening to connectivity changes

--- Drive.Client/Drive.Client/Services/Signal/Announcement/AnnouncementSignalService.cs
+++ Drive.Client/Drive.Client/Services/Signal/Announcement/AnnouncementSignalService.cs
@@ -37,104 +37,137 @@ namespace Drive.Client.Services.Signal.Announcement {
         public override string SocketHubGateway { get; protected set; } = BaseSingleton<GlobalSetting>.Instance.RestEndpoints.SignalGateways.Announcements;
 
         protected override void OnStartListeningToHub() {
-            _hubConnection.On<object>(UPDATE_POST_LIKES_COUNT, async (args) => {
-                try {
+            _hubConnection.On<object>(UPDATE_POST_LIKES_COUNT, async (args) =>
+            {
+                try
+                {
                     DrivenEventResponse drivenEventResponse = ParseResponseData<DrivenEventResponse>(args);
 
-                    if (drivenEventResponse.StatusCode == HttpStatusCode.OK) {
+                    if (drivenEventResponse.StatusCode == HttpStatusCode.OK)
+                    {
                         DrivenEvent drivenEvent = ParseResponseData<DrivenEvent>(drivenEventResponse.Data);
 
                         PostLikedBody postLikedBody = ParseResponseData<PostLikedBody>(drivenEvent.Data);
                         PostLikesCountReceived(this, postLikedBody);
-                    } else if (drivenEventResponse.StatusCode == HttpStatusCode.Unauthorized) {
+                    }
+                    else if (drivenEventResponse.StatusCode == HttpStatusCode.Unauthorized)
+                    {
                         await DependencyLocator.Resolve<IIdentityService>().LogOutAsync();
                     }
                 }
-                catch (Exception ex) {
+                catch (Exception ex)
+                {
                     Crashes.TrackError(ex);
                 }
             });
 
-            _hubConnection.On<object>(REMOVE_POST, async (args) => {
-                try {
+            _hubConnection.On<object>(REMOVE_POST, async (args) =>
+            {
+                try
+                {
                     DrivenEventResponse drivenEventResponse = ParseResponseData<DrivenEventResponse>(args);
 
-                    if (drivenEventResponse.StatusCode == HttpStatusCode.OK) {
+                    if (drivenEventResponse.StatusCode == HttpStatusCode.OK)
+                    {
                         DrivenEvent drivenEvent = ParseResponseData<DrivenEvent>(drivenEventResponse.Data);
 
                         string postId = ParseResponseData<string>(drivenEvent.Data);
                         DeletedPostReceived(this, postId);
-                    } else if (drivenEventResponse.StatusCode == HttpStatusCode.Unauthorized) {
+                    }
+                    else if (drivenEventResponse.StatusCode == HttpStatusCode.Unauthorized)
+                    {
                         await DependencyLocator.Resolve<IIdentityService>().LogOutAsync();
                     }
                 }
-                catch (Exception ex) {
+                catch (Exception ex)
+                {
                     Crashes.TrackError(ex);
                 }
             });
 
-            _hubConnection.On<object>(NEW_POST_COMMENT, async (args) => {
-                try {
+            _hubConnection.On<object>(NEW_POST_COMMENT, async (args) =>
+            {
+                try
+                {
                     DrivenEventResponse drivenEventResponse = ParseResponseData<DrivenEventResponse>(args);
 
-                    if (drivenEventResponse.StatusCode == HttpStatusCode.OK) {
+                    if (drivenEventResponse.StatusCode == HttpStatusCode.OK)
+                    {
                         DrivenEvent drivenEvent = ParseResponseData<DrivenEvent>(drivenEventResponse.Data);
 
                         Comment comment = ParseResponseData<Comment>(drivenEvent.Data);
                         NewPostCommentReceived(this, comment);
-                    } else if (drivenEventResponse.StatusCode == HttpStatusCode.Unauthorized) {
+                    }
+                    else if (drivenEventResponse.StatusCode == HttpStatusCode.Unauthorized)
+                    {
                         await DependencyLocator.Resolve<IIdentityService>().LogOutAsync();
                     }
                 }
-                catch (Exception ex) {
+                catch (Exception ex)
+                {
                     Crashes.TrackError(ex);
                 }
             });
 
-            _hubConnection.On<object>(UPDATE_POST_COMMENTS_C0UNT, async (args) => {
-                try {
+            _hubConnection.On<object>(UPDATE_POST_COMMENTS_C0UNT, async (args) =>
+            {
+                try
+                {
                     DrivenEventResponse drivenEventResponse = ParseResponseData<DrivenEventResponse>(args);
 
-                    if (drivenEventResponse.StatusCode == HttpStatusCode.OK) {
+                    if (drivenEventResponse.StatusCode == HttpStatusCode.OK)
+                    {
                         DrivenEvent drivenEvent = ParseResponseData<DrivenEvent>(drivenEventResponse.Data);
 
                         CommentCountBody commentCountBody = ParseResponseData<CommentCountBody>(drivenEvent.Data);
                         PostCommentsCountReceived.Invoke(this, commentCountBody);
-                    } else if (drivenEventResponse.StatusCode == HttpStatusCode.Unauthorized) {
+                    }
+                    else if (drivenEventResponse.StatusCode == HttpStatusCode.Unauthorized)
+                    {
                         await DependencyLocator.Resolve<IIdentityService>().LogOutAsync();
                     }
                 }
-                catch (Exception ex) {
+                catch (Exception ex)
+                {
                     Crashes.TrackError(ex);
                 }
             });
 
-            _hubConnection.On<object>(NEW_ANNOUNCE, async (args) => {
-                try {
+            _hubConnection.On<object>(NEW_ANNOUNCE, async (args) =>
+            {
+                try
+                {
                     Console.WriteLine("===> AnnouncementHubService.NewPostHubEndpoint <===");
                     DrivenEventResponse drivenEventResponse = ParseResponseData<DrivenEventResponse>(args);
 
-                    if (drivenEventResponse.StatusCode == HttpStatusCode.OK) {
+                    if (drivenEventResponse.StatusCode == HttpStatusCode.OK)
+                    {
                         DrivenEvent drivenEvent = ParseResponseData<DrivenEvent>(drivenEventResponse.Data);
 
                         Announce announce = ParseResponseData<Announce>(drivenEvent.Data);
-                        if (announce != null && announce.AnnounceBody != null && announce.ImageUrl != null) {
+                        if (announce != null && announce.AnnounceBody != null && announce.ImageUrl != null)
+                        {
                             NewAnnounceReceived.Invoke(this, announce);
-                        } else  {
+                        }
+                        else
+                        {
                             announce = new Announce();
                             AnnounceBody announceBody = ParseResponseData<AnnounceBody>(drivenEvent.Data);
                             announce.AnnounceBody = announceBody;
                             NewAnnounceReceived.Invoke(this, announce);
                         }
-                    } else if (drivenEventResponse.StatusCode == HttpStatusCode.Unauthorized) {
+                    }
+                    else if (drivenEventResponse.StatusCode == HttpStatusCode.Unauthorized)
+                    {
                         await DependencyLocator.Resolve<IIdentityService>().LogOutAsync();
                     }
                 }
-                catch (Exception exc) {
+                catch (Exception exc)
+                {
                     Crashes.TrackError(exc);
                     Debugger.Break();
                 }
-            });           
+            });
         }
     }
 }
