ffa82124793a2381c699e1f4aa22b34e7049df52
vadym.stetsyuk.89 <vadym.stetsyuk.89@gmail.com>
2019-01-24 18:16:20 +0200

    Dropdown selector controls

--- Drive.Client/Drive.Client/ViewModels/BottomTabViewModels/Calculator/CalculatorViewModel.cs
+++ Drive.Client/Drive.Client/ViewModels/BottomTabViewModels/Calculator/CalculatorViewModel.cs
@@ -1,31 +1,13 @@
 ﻿using Drive.Client.Helpers;
 using Drive.Client.ViewModels.Base;
 using Drive.Client.Views.BottomTabViews.Calculator;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Drive.Client.ViewModels.BottomTabViewModels.Calculator {
     public sealed class CalculatorViewModel : TabbedViewModelBase {
 
-        /// <summary>
-        ///     ctor().
-        /// </summary>
-        public CalculatorViewModel() {
-
-        }
-
         public override void Dispose() {
             base.Dispose();
-        }
-
-        public override Task InitializeAsync(object navigationData) {
-
-
-
-
-            return base.InitializeAsync(navigationData);
         }
 
         protected override void TabbViewModelInit() {
